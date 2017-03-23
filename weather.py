a06771a829eeb61d89894f55cce78d9ac91be20f
eternalxing <xing_yongheng@163.com>
2017-03-23 09:54:11 +0800

    天气爬虫

--- weather.py
+++ weather.py
@@ -2,18 +2,26 @@
 from bs4 import BeautifulSoup
 import urllib2
 import sys
+import time
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+month_list=['01','02','03','04','05','06','07','08','09','10','11','12']
 f = open("F:/1.txt", "w+")
-url = 'http://lishi.tianqi.com/changchun/201612.html'
-request = urllib2.Request(url)
-content = urllib2.urlopen(request).read()
-soup = BeautifulSoup(content, "html.parser", from_encoding="gb18030")
-data = soup.find('div', class_='tqtongji2').find_all(['ul'])
-for ul in data[1:]:
-    data1=ul.find_all(['li'])
-    for i in data1[0:3]:
-        f.write(i.string)
+for year in range(2011,2016):
+    for month in month_list:
+        str1=str(year)+month
+        url = 'http://lishi.tianqi.com/changchun/'+str1+'.html'
+        request = urllib2.Request(url)
+        content = urllib2.urlopen(request).read()
+        soup = BeautifulSoup(content, "html.parser", from_encoding="gb18030")
+        data = soup.find('div', class_='tqtongji2').find_all(['ul'])
+        for ul in data[1:]:
+            data1=ul.find_all(['li'])
+            for i in data1[0:3]:
+                f.write(i.string+" ")
+            f.write('\n')
+    time.sleep(1)
+f.close()
 
