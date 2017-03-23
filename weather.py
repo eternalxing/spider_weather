1f26a81939a46c38d7b9dd3d13c7b9b2cab7abab
eternalxing <xing_yongheng@163.com>
2017-03-23 09:17:59 +0800

    one day

--- weather.py
+++ weather.py
@@ -11,8 +11,9 @@ url = 'http://lishi.tianqi.com/changchun/201612.html'
 request = urllib2.Request(url)
 content = urllib2.urlopen(request).read()
 soup = BeautifulSoup(content, "html.parser", from_encoding="gb18030")
-data = soup.find('div', class_='tqtongji2').find_all(['li'])
-str1=" "
-for i in data:
-    str1 = str1 + i.string
-print str1[2]
+data = soup.find('div', class_='tqtongji2').find_all(['ul'])
+for ul in data[1:]:
+    data1=ul.find_all(['li'])
+    for i in data1[0:3]:
+        f.write(i.string)
+
